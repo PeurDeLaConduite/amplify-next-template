8c497ea987c5a567a7debc050111f945b4d74257
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-14 15:40:04 +0200

    Base 2

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -5,7 +5,7 @@ const schema = a.schema({
         .model({
             content: a.string(),
         })
-        .authorization((allow) => [allow.publicApiKey()]),
+        .authorization((allow) => [allow.owner(), allow.publicApiKey().to(["read"])]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -14,37 +14,9 @@ export const data = defineData({
     schema,
     authorizationModes: {
         defaultAuthorizationMode: "apiKey",
+        // API Key is used for allow.publicApiKey() rules
         apiKeyAuthorizationMode: {
             expiresInDays: 30,
         },
     },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
