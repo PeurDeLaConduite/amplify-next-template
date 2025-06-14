b00a6ef4cb1ab6a3a3a1dcaedf6beeee2a502f41
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-14 16:03:31 +0200

    BAse 2 t

---
+++ amplify-backup/data/resource.ts
@@ -0,0 +1,22 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+    Todo: a
+        .model({
+            content: a.string(),
+        })
+        .authorization((allow) => [allow.owner(), allow.publicApiKey().to(["read"])]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+    schema,
+    authorizationModes: {
+        defaultAuthorizationMode: "apiKey",
+        // API Key is used for allow.publicApiKey() rules
+        apiKeyAuthorizationMode: {
+            expiresInDays: 30,
+        },
+    },
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -13,40 +14,47 @@ const client = generateClient<Schema>();
 
 export default function App() {
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-    const { user, signOut } = useAuthenticator();
 
-    const groups = user?.signInUserSession?.accessToken?.payload?.["cognito:groups"] || [];
-    const isAdmin = groups.includes("ADMINS");
-
-    useEffect(() => {
+    function listTodos() {
         client.models.Todo.observeQuery().subscribe({
             next: (data) => setTodos([...data.items]),
         });
-    }, []);
-
-    function createTodo() {
-        const content = window.prompt("Todo content");
-        if (content) {
-            client.models.Todo.create({ content });
-        }
     }
 
     function deleteTodo(id: string) {
         client.models.Todo.delete({ id });
     }
+    useEffect(() => {
+        listTodos();
+    }, []);
+
+    function createTodo() {
+        client.models.Todo.create({
+            content: window.prompt("Todo content"),
+        });
+    }
+    const { user, signOut } = useAuthenticator();
 
     return (
         <main>
-            <h1>{isAdmin ? "Admin" : "User"}'s Todos</h1>
-            {isAdmin && <button onClick={createTodo}>+ new</button>}
+            <h1>{user?.signInDetails?.loginId} s todos</h1>
+            <button onClick={createTodo}>+ new</button>
             <ul>
                 {todos.map((todo) => (
                     <li key={todo.id}>
                         {todo.content}
-                        {isAdmin && <button onClick={() => deleteTodo(todo.id)}>Delete</button>}
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
+            <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+                    Review next steps of this tutorial.
+                </a>
+            </div>
+
             <button onClick={signOut}>Sign out</button>
         </main>
     );
