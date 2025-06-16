1375f441579ae99fb8c381ba82615b8d353338c6
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-16 23:34:54 +0200

    feature Base One

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
-import { deleteTodoWithComments } from "./functions/delete-todo/resource.js"; // 👈
 
 defineBackend({
     auth,
     data,
-    deleteTodoWithComments, // 👈 N'OUBLIE PAS
 });
