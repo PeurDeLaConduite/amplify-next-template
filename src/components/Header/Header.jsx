2de92355f19cf02fdd4f289cb53cac0f2e813152
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-06 12:36:32 +0200

    feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useUserName } from "@src/entities";
 import { PowerButton } from "../buttons/Buttons";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import UserNameModal from "../Profile/UserNameModal";
 
 const Header = () => {
     const { user, signOut } = useAuthenticator();
     const { userName } = useUserName();
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        if (user && !userName) {
+            setShowModal(true);
+        }
+        if (userName) {
+            setShowModal(false);
+        }
+    }, [user, userName]);
+
     return (
-        <header className="bg-white shadow-md">
-            <nav className="max-w-6xl mx-auto flex items-center justify-between p-4">
-                <div className="flex gap-6">
-                    {/* <Link href="/comment" className="text-gray-700 hover:text-blue-600">
+        <>
+            <header className="bg-white shadow-md">
+                <nav className="max-w-6xl mx-auto flex items-center justify-between p-4">
+                    <div className="flex gap-6">
+                        {/* <Link href="/comment" className="text-gray-700 hover:text-blue-600">
                         Commentaires
                     </Link>
 
@@ -23,69 +36,69 @@ const Header = () => {
                     <Link href="/uploadPage" className="text-gray-700 hover:text-blue-600">
                         Upload Page
                     </Link> */}
-                    <Link href="/todo" className="text-gray-700 hover:text-blue-600">
-                        Todo
-                    </Link>
-                    <Link href="/createAuthor" className="text-gray-700 hover:text-blue-600">
-                        Create Author
-                    </Link>
+                        <Link href="/todo" className="text-gray-700 hover:text-blue-600">
+                            Todo
+                        </Link>
+                        <Link href="/createAuthor" className="text-gray-700 hover:text-blue-600">
+                            Create Author
+                        </Link>
+                        <Link href="/createSection" className="text-gray-700 hover:text-blue-600">
+                            Create Section
+                        </Link>
+                        <Link href="/createTag" className="text-gray-700 hover:text-blue-600">
+                            Create Tag
+                        </Link>
+                        <Link href="/createArticle" className="text-gray-700 hover:text-blue-600">
+                            Create Article
+                        </Link>{" "}
+                        <Link href="/blog" className="text-gray-700 hover:text-blue-600">
+                            Blog
+                        </Link>
+                        <Link href="/profile" className="text-gray-700 hover:text-blue-600">
+                            My profile
+                        </Link>
+                    </div>
 
-                    <Link href="/createSection" className="text-gray-700 hover:text-blue-600">
-                        Create Section
-                    </Link>
-                    <Link href="/createTag" className="text-gray-700 hover:text-blue-600">
-                        Create Tag
-                    </Link>
-                    <Link href="/createArticle" className="text-gray-700 hover:text-blue-600">
-                        Create Article
-                    </Link>{" "}
-                    <Link href="/blog" className="text-gray-700 hover:text-blue-600">
-                        Blog
-                    </Link>
-                    <Link href="/profile" className="text-gray-700 hover:text-blue-600">
-                        My profile
-                    </Link>
-                </div>
-
-                <div className="flex items-center gap-4">
-                    {user ? (
-                        userName ? (
-                            <>
-                                <p className="text-sm text-gray-700">
-                                    Connecté en tant que : <strong>{userName}</strong>
-                                </p>
-                                <Link
-                                    href="/connection"
-                                    className="text-gray-700 hover:text-blue-600"
-                                >
+                    <div className="flex items-center gap-4">
+                        {user ? (
+                            userName ? (
+                                <>
+                                    <p className="text-sm text-gray-700">
+                                        Connecté en tant que : <strong>{userName}</strong>
+                                    </p>
+                                    <Link
+                                        href="/connection"
+                                        className="text-gray-700 hover:text-blue-600"
+                                    >
+                                        <PowerButton
+                                            onClick={signOut}
+                                            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
+                                        >
+                                            Se déconnecter
+                                        </PowerButton>
+                                    </Link>
+                                </>
+                            ) : (
+                                <>
+                                    <span className="text-sm text-gray-400">Chargement...</span>{" "}
                                     <PowerButton
                                         onClick={signOut}
                                         className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
                                     >
                                         Se déconnecter
-                                    </PowerButton>
-                                </Link>
-                            </>
+                                    </PowerButton>{" "}
+                                </>
+                            )
                         ) : (
-                            <>
-                                <span className="text-sm text-gray-400">Chargement...</span>{" "}
-                                <PowerButton
-                                    onClick={signOut}
-                                    className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-                                >
-                                    Se déconnecter
-                                </PowerButton>{" "}
-                            </>
-                        )
-                    ) : (
-                        <Link href="/connection" className="text-gray-700 hover:text-blue-600">
-                            Connection
-                        </Link>
-                    )}
-                </div>
-            </nav>
-        </header>
+                            <Link href="/connection" className="text-gray-700 hover:text-blue-600">
+                                Connection
+                            </Link>
+                        )}
+                    </div>
+                </nav>
+            </header>
+            <UserNameModal isOpen={showModal} onClose={() => setShowModal(false)} />
+        </>
     );
 };
-
 export default Header;
