d9cd82637ea3b3f57059163b0137862482a8b3d6
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-09 10:46:40 +0200

    feature

--- scripts/generator/generator.config.ts
+++ scripts/generator/generator.config.ts
@@ -1,23 +1,24 @@
 export const GEN = {
-  schemaRel: "amplify/data/resource.ts",
-  tsconfigRel: "tsconfig.json",
-  out: {
-    models: "src/entities/models",
-    customTypes: "src/entities/customTypes",
-    introspection: "src/entities/introspection",
-  },
-  paths: {
-    myTypes: "@myTypes/amplifyBaseTypes",
-    createModelForm: "@src/entities/core/createModelForm",
-    crudService: "@src/services",
-    createEntityHooks: "@src/entities/core/createEntityHooks",
-    customTypeFormDir: (refType: string) =>
-      `@src/entities/customTypes/${refType.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase()}/form`,
-  },
-  rules: {
-    includeIdInForm: true,
-    omitSystemFields: true,
-    exposeHasManyIds: {} as Record<string, string[]>,
-  },
+    schemaRel: "amplify/data/resource.ts",
+    tsconfigRel: "tsconfig.json",
+    out: {
+        models: "src/entities/models",
+        customTypes: "src/entities/customTypes",
+        introspection: "src/entities/introspection",
+        relations: "src/entities/relations",
+    },
+    paths: {
+        myTypes: "@myTypes/amplifyBaseTypes",
+        createModelForm: "@src/entities/core/createModelForm",
+        crudService: "@src/services",
+        relationService: "@src/entities/core/services/relationService",
+        createEntityHooks: "@src/entities/core/createEntityHooks",
+        customTypeFormDir: (refType: string) =>
+            `@src/entities/customTypes/${refType.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase()}/form`,
+    },
+    rules: {
+        includeIdInForm: true,
+        omitSystemFields: true,
+        exposeHasManyIds: {} as Record<string, string[]>,
+    },
 } as const;
-
