f3ae08866acc6c5a3faf3d704a515af551924dd8
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-10 21:16:57 +0200

    feature

--- src/components/Profile/shared/EntitySection.tsx
+++ src/components/Profile/shared/EntitySection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import type { FieldKey, EntityManagerResult } from "@src/entities/core";
+import type { FieldKey, EntityManagerResult } from "@src/entities/core/hooks";
 import ReadOnlyView from "./ReadOnlyView";
 import EditField from "./EditField";
 import EntityForm from "./EntityForm";
