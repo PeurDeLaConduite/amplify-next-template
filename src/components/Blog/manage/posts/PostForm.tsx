3a35a3b5fc6f5000aab4961ef6fb5143f013def8
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-12 14:33:44 +0200

    feature

--- src/components/Blog/manage/posts/PostForm.tsx
+++ src/components/Blog/manage/posts/PostForm.tsx
@@ -190,19 +190,7 @@ const PostForm = forwardRef<HTMLFormElement, Props>(function SectionForm(
                     ))}
             </fieldset>
             <div className="flex space-x-2">
-                <button
-                    type="button"
-                    onClick={reset}
-                    disabled={!dirty || saving}
-                    className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
-                >
-                    Réinitialiser
-                </button>
-                <button
-                    type="submit"
-                    disabled={saving}
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
                     {post ? "Mettre à jour" : "Créer"}
                 </button>
             </div>
