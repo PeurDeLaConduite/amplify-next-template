4f68bc01bd0e7538bee192d396003029b068bb2b
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-13 12:27:04 +0200

    Feat: add policy-based services

--- src/entities/models/section/service.ts
+++ src/entities/models/section/service.ts
@@ -1,3 +1,13 @@
 import { crudService } from "@entities/core";
+import type { AuthUser, SimplePolicy } from "@entities/core/types";
+import { expandPolicy } from "@entities/core/auth";
 
-export const sectionService = crudService("Section");
+const policy: SimplePolicy = {
+    read: ["public", "private"],
+    create: { groups: ["ADMINS"] },
+    update: { groups: ["ADMINS"] },
+    delete: { groups: ["ADMINS"] },
+};
+const rules = expandPolicy(policy);
+
+export const sectionService = (user: AuthUser | null) => crudService("Section", user, rules);
--- src/entities/models/tag/service.ts
+++ src/entities/models/tag/service.ts
@@ -1,3 +1,13 @@
 import { crudService } from "@entities/core";
+import type { AuthUser, SimplePolicy } from "@entities/core/types";
+import { expandPolicy } from "@entities/core/auth";
 
-export const tagService = crudService("Tag");
+const policy: SimplePolicy = {
+    read: ["public", "private"],
+    create: { groups: ["ADMINS"] },
+    update: { groups: ["ADMINS"] },
+    delete: { groups: ["ADMINS"] },
+};
+const rules = expandPolicy(policy);
+
+export const tagService = (user: AuthUser | null) => crudService("Tag", user, rules);
