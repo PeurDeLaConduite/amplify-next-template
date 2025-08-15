96806dcf4017914d41af8d914abc7aa9473fa839
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-15 14:41:35 +0200

    fix: ajuster les types et service UserName

--- src/entities/models/userName/types.ts
+++ src/entities/models/userName/types.ts
@@ -2,6 +2,7 @@ import type { BaseModel, CreateOmit, UpdateInput, ModelForm } from "@entities/co
 
 export type UserNameType = BaseModel<"UserName">;
 export type UserNameTypeOmit = CreateOmit<"UserName">;
-export type UserNameTypeUpdateInput = UpdateInput<"UserName">;
+export type UserNameTypeCreateInput = UpdateInput<"UserName">;
+export type UserNameTypeUpdateInput = { id: string } & Partial<UserNameTypeCreateInput>;
 export type UserNameFormType = ModelForm<"UserName", "comments", "comments" | "postComments">;
 export type UserNameMinimalType = { userName: string };
