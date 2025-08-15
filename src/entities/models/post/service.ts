b29daaedafc287f734e595b2d66ce2ed577eb655
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-15 14:39:51 +0200

    feat: ajuste les types et le service du modèle Post

--- src/entities/models/post/service.ts
+++ src/entities/models/post/service.ts
@@ -1,5 +1,13 @@
 import { crudService } from "@entities/core";
+import type { IdArg } from "@entities/core/types";
+import type { PostTypeCreateInput, PostTypeUpdateInput } from "./types";
 
-export const postService = crudService("Post", {
+export const postService = crudService<
+    "Post",
+    PostTypeCreateInput,
+    PostTypeUpdateInput,
+    IdArg,
+    IdArg
+>("Post", {
     auth: { read: ["apiKey", "userPool"], write: "userPool" },
 });
--- src/entities/models/post/types.ts
+++ src/entities/models/post/types.ts
@@ -1,9 +1,9 @@
-import type { BaseModel, CreateOmit, UpdateInput, ModelForm } from "@entities/core";
+import type { BaseModel, CreateOmit, ModelForm } from "@entities/core/types";
 import { type SeoTypeOmit } from "@entities/customTypes/seo/types";
 
 export type PostType = BaseModel<"Post">;
-export type PostTypeOmit = CreateOmit<"Post">;
-export type PostTypeUpdateInput = UpdateInput<"Post">;
+export type PostTypeCreateInput = CreateOmit<"Post">;
+export type PostTypeUpdateInput = { id: string } & Partial<PostTypeCreateInput>;
 
 type PostCustomTypes = { seo: SeoTypeOmit };
 export type PostFormType = ModelForm<
