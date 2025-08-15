66336d76c9acbb755217e15820d29f0158986a15
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-15 14:42:16 +0200

    feat: refine user profile types and service

--- src/entities/models/userProfile/service.ts
+++ src/entities/models/userProfile/service.ts
@@ -1,5 +1,14 @@
 // src/entities/models/userProfile/service.ts
-import { crudService } from "@entities/core/services/crudService";
-export const userProfileService = crudService("UserProfile", {
-    auth: { read: "userPool", write: "userPool" },
+import { crudService } from "@entities/core";
+import type { UserProfileTypeCreateInput, UserProfileTypeUpdateInput } from "./types";
+import type { IdArg } from "@entities/core/types";
+
+export const userProfileService = crudService<
+    "UserProfile",
+    UserProfileTypeCreateInput,
+    UserProfileTypeUpdateInput,
+    IdArg,
+    IdArg
+>("UserProfile", {
+    auth: { read: ["apiKey", "userPool"], write: "userPool" },
 });
