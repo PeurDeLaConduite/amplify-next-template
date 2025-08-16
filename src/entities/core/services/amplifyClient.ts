d027073bfc7e2f6dd79f26ac4e378508fed5fe5b
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-16 20:17:49 +0200

    feat: enable multi-auth strategy for Amplify client

--- src/entities/core/services/amplifyClient.ts
+++ src/entities/core/services/amplifyClient.ts
@@ -2,9 +2,12 @@
 
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
+import { AuthModeStrategyType } from "@aws-amplify/datastore";
 import outputs from "@/amplify_outputs.json";
 import type { Schema } from "@/amplify/data/resource";
 
 Amplify.configure(outputs);
-export const client = generateClient<Schema>();
+export const client = generateClient<Schema>({
+    authModeStrategyType: AuthModeStrategyType.MULTI,
+});
 export type { Schema } from "@/amplify/data/resource";
