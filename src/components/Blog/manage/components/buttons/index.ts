c773ffb183ab277e78e0392c80c612b50b44ee44
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-17 21:03:37 +0200

    refactor: remove unused manage buttons index

--- src/components/Blog/manage/components/buttons/index.ts
+++
@@ -1,3 +0,0 @@
-import { EditButton, SaveButton, CancelButton } from "@components/buttons";
-// tu peux aussi exporter DeleteButton, AddButton, etc.
-export { EditButton, SaveButton, CancelButton };
