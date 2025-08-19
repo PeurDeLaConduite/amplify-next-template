12ba1e7917e073b55162e1a378dd34b83eb2cc49
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-19 03:09:04 +0200

    Fix: corrige OperationArg

--- src/entities/core/services/crudService.ts
+++ src/entities/core/services/crudService.ts
@@ -8,8 +8,12 @@ type ClientModels = typeof client.models;
 type ClientModelKey = keyof ClientModels;
 
 type BaseModel<K extends ClientModelKey> = Schema[K]["type"];
-type OperationArg<T, M extends PropertyKey> =
-    T extends Record<M, (arg: infer A, ...rest: unknown[]) => unknown> ? A : never;
+type OperationArg<T, M extends PropertyKey> = T[M] extends (
+    arg: infer A,
+    ...rest: unknown[]
+) => unknown
+    ? A
+    : never;
 
 type CreateArg<K extends ClientModelKey> = OperationArg<ClientModels[K], "create">;
 type UpdateArg<K extends ClientModelKey> = OperationArg<ClientModels[K], "update">;
