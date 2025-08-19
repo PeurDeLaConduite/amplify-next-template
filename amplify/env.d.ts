55b5bbb77e07e468501d666249077e97d9ebea38
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-08-19 02:21:39 +0200

    fix(amplify): use string env record

--- amplify/env.d.ts
+++ amplify/env.d.ts
@@ -1,4 +1,3 @@
 declare module "$amplify/env/delete-todo" {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    export const env: Record<string, any>;
+    export const env: Record<string, string>;
 }
