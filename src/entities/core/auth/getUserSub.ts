8c80654ef42fbe80f8ba7122aad0f039fd7e95c6
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-21 06:02:08 +0200

    FEAT

--- src/entities/core/auth/getUserSub.ts
+++ src/entities/core/auth/getUserSub.ts
@@ -4,3 +4,9 @@ export async function getUserSub(): Promise<string> {
     const { userId } = await getCurrentUser();
     return userId;
 }
+// src/entities/core/auth/getUserSub.ts
+
+// export async function getUserSub(): Promise<string> {
+//     const u = await getCurrentUser();
+//     return u.userId ?? (u as any)?.attributes?.sub ?? u.username; // robustesse
+// }
--- src/entities/core/index.ts
+++ src/entities/core/index.ts
@@ -2,6 +2,6 @@ export * from "./hooks";
 export * from "./services";
 export * from "./utils";
 export * from "./types";
-export * from "./auth";
+export * from "./auth/getUserSub";
 export * from "./managerContract";
 export * from "./createManager";
