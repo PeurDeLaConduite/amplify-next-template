03ea5972d6e616f429de518ca0084d84b9a5a459
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-21 17:09:26 +0200

    FEAT

--- amplify/env.d.ts
+++ amplify/env.d.ts
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
 declare module "$amplify/env/delete-todo" {
     export const env: Record<string, string>;
 }
-=======
-declare module "$amplify/env/delete-todo" {
-    export const env: Record<string, string>;
-}
->>>>>>> Stashed changes
